9533ce1422482902b3e0a50ec880ef07e81358fe
Robert Gathanua <robertgathanua@gamail.com>
2024-04-24 06:27:59 +0300

    complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return <nav>
+      <a id="home" href="#home">Home</a>
+      <a id="about" href="#about">About</a>
+  </nav>;
 }
 
 export default NavBar;
